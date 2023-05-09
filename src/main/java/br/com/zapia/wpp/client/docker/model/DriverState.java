d967645c979b43e02f4a10d64950eb1a5175a0bf
bobaoapae <jvborges@outlook.com>
2023-05-09 18:31:56 -0300

    WAITING_QR_OR_LOGIN

--- src/main/java/br/com/zapia/wpp/client/docker/model/DriverState.java
+++ src/main/java/br/com/zapia/wpp/client/docker/model/DriverState.java
@@ -3,6 +3,7 @@ package br.com.zapia.wpp.client.docker.model;
 public enum DriverState {
     UNLOADED,
     LOADING,
+    WAITING_QR_OR_LOGIN,
     WAITING_QR_CODE_SCAN,
     QR_CODE_SCANNED,
     LOADING_STORE,
